cc6b9e3119f91e7b07b75a21046198793c343f12
Pat Williams <patrickwilliams1414@gmail.com>
2017-06-30 19:52:30 +0000

    Trigger the mailer from the controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,6 +10,16 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
+      # We have to collect the name, email and body from the form fields
+      # The Word params related to stuff in form fields/links.
+      # Code right below: This is how we grab it..you use params to do this
+      # This is how you lift from form fields in controller
+      # Go into contact form after saved and grab all these form fields, assign to 
+      #variables and use in next line
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message Sent!"
       redirect_to new_contact_path
     else
