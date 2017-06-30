c247854f3a5b074e22711071610f59f6c3ae546a
Pat Williams <patrickwilliams1414@gmail.com>
2017-06-30 18:31:32 +0000

    Added contact form mailer file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  
+  # Only use create action
   resources :contacts, only: :create
   
   #Custom URL for contact page.
   #instead of original: '/contacts/new' changing it to: 'contact-us'
   # Then pointing to the contacts controller, new action
   
+  # the 'new_contact' comes from routes file.  Do 'rails routes' and see prefix
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
