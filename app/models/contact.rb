d0842f697d64c3586e5bc74123e0e6c63707612e
Pat Williams <patrickwilliams1414@gmail.com>
2017-06-30 16:50:09 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
 class Contact < ActiveRecord::Base
-    
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
 
 # Taking data from input form fields and saving it to the database
