5db8143fb6ede13a28b6d300795b301c9b891053
Pat Williams <patrickwilliams1414@gmail.com>
2017-06-30 19:43:13 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,12 @@
 class ContactMailer < ApplicationMailer::Based
   default to: 'patrickwilliams1414@gmail.com'
+  
   def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
     mail(from: email, subject: 'Contact Form Message')
   end
-
 end
+
+# Since we're dealing with the contact form, we called this file ContactMailer
