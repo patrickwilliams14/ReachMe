4f6116f75dc6a96a6f1b4e28bf0aff9bfa0b8783
Pat Williams <patrickwilliams1414@gmail.com>
2017-06-30 18:14:57 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :contacts
+  resources :contacts, only: :create
   
   #Custom URL for contact page.
   #instead of original: '/contacts/new' changing it to: 'contact-us'
   # Then pointing to the contacts controller, new action
-  get 'contact-us', to: 'contacts#new'
+  
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
