5fd3e57e94e4b30d0463b2da757a403bf15e4899
Pat Williams <patrickwilliams1414@gmail.com>
2018-01-07 03:17:34 +0000

    Editede register schools js file

--- app/assets/javascripts/register_schools.js
+++ app/assets/javascripts/register_schools.js
@@ -1,12 +1,15 @@
-/* global $, Stripe */
+/* global $ Stripe */
 //Document ready.
 $(document).on('turbolinks:load', function(){
   
-  var theForm = $('#payment_form');
+  var theForm = $('#payment-form');
   var submitBtn = $('#form-signup-btn');
   
   //Set Stripe public key.
-  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  var stripe = Stripe($('meta[name="stripe-key"]').attr('content'));
+  
+  //Use JS Elements
+  var elements = stripe.elements();
   
   //When user clicks form submit btn,
   submitBtn.click(function(event){
@@ -25,29 +28,31 @@ $(document).on('turbolinks:load', function(){
     var error = false;
     
     //Validate card number.
-    if(!Stripe.card.validateCardNumber(ccNum)) {
+    if(!stripe.card.validateCardNumber(ccNum)) {
       error = true;
       alert('The credit card number appears to be invalid');
     }
     
     //Validate CVC number.
-    if(!Stripe.card.validateCVC(cvcNum)) {
+    if(!stripe.card.validateCVC(cvcNum)) {
       error = true;
       alert('The CVC number appears to be invalid');
     }
     
     //Validate expiration date.
-    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+    if(!stripe.card.validateExpiry(expMonth, expYear)) {
       error = true;
       alert('The expiration date appears to be invalid');
     }
     
     if (error) {
+      
       //If there are card errors, don't send to Stripe.
-      submitBtn.prop('disabled', false).val("Sign Up");
+      submitBtn.prop('disabled', false).val("Get ReachMe!");
     } else {
+      
       //Send the card info to Stripe.
-      Stripe.createToken({
+      stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
@@ -61,10 +66,13 @@ $(document).on('turbolinks:load', function(){
   
   //Stripe will return a card token.
   function stripeResponseHandler(status, response) {
+    
     //Get the token from the response.
     var token = response.id;
+    
     //Inject the card token in a hidden field.
-    theForm.append( $('<input type="hidden" name="registeredschool[stripe_card_token]">').val(token) );
+    theForm.append( $('<input type="hidden" name="school_registration[stripe_card_token]">').val(token) );
+    
     //Submit form to our Rails app.
     theForm.get(0).submit();
   }
